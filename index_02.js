e6a6cd3d4c31c2afac74f50881737c6e28966bb2
SurkusGH <v.surkus@me.com>
2022-05-04 21:36:20 +0300

    Update

--- index_02.js
+++ index_02.js
@@ -7,3 +7,24 @@ document.body.append(paragraph)
 const div = document.querySelector('div')
 
 div.prepend(paragraph)
+
+// --- --- ---
+
+const tempPara1 = document.createElement('p')
+tempPara1.textContent = "0-99!"
+paragraph.style.backgroundColor = "lightcoral"
+document.body.prepend(tempPara1)
+
+const inputField = document.querySelector("#number");
+
+inputField.addEventListener('focus', (event) => {
+    //event.target.style.background = 'lightblue';
+    if(val < 99){
+        document.body.prepend(tempPara1)
+      }
+    if(val > 99){
+        document.body.append(tempPara1)
+      }
+    });
+
+const val = document.querySelector("#number").Value;
